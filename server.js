38195cdc1142f8e0fe462d51094dc315e9066ad6
slpencer <susan.spencer@gmail.com>
2022-08-23 22:36:20 -0500

    update app setup

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
-// Install express
+// Install express, apiRoutes, and htmlRoutes
 const express = require('express');
-const app = express();
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
 
 // Install fs & path to read files
 const fs = require('fs');
 const path = require('path');
 
+// Init the express app
+const app = express();
+
 // listen on port 3306 (heroku)
 const PORT = process.env.PORT || 3306;
 
-// create routers
-const apiRoutes = require('./routes/apiRoutes');
-const htmlROutes = require('.routes/htmlRoutes');
-
-// define middleware with express
+// setup middleware
+app.use(express.json());
 app.use(express.urlencoded( { extended: true } ));
 
 // set up directory for static files & assets
 app.use(express.static('public'));
-// ??
-app.use(expresss.json());
 
 // connect application to express routes
 app.use('/api', apiRoutes);
-app.use('/', json());
+app.use('/', htmlRoutes);
 
-// listen to connections on host & port
+// start server on port
 app.listen(PORT, () => {
     console.log(`Note taking app listening at http://localhost:${port}`);
 });
