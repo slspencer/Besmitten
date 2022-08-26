611d9f7cec6f9e45115fb83c9d2c07a1d76a4f23
slpencer <susan.spencer@gmail.com>
2022-08-26 04:34:49 -0500

    relative URL htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,27 +8,27 @@ var serveIndex = require('serve-index');
 module.exports = (app) => {
 
     // list files in current directory as troubleshooting measure
-    app.use(express.static(__dirname + "/"))
-    app.use('/videos', serveIndex(__dirname + '/videos'));
+    //app.use(express.static(__dirname + "/"))
+   //app.use('/', serveIndex(__dirname + '/');
 
     // initial page load "/" serves index.html
     app.get("/", (req, res) => {
-        console.log("HTML Route to ./public/index.html")
-        res.sendFile(path.join(__dirname, "./public/index.html"));
+        console.log("HTML Route to ../public/index.html")
+        res.sendFile(path.join("../public/index.html"));
     });
 
     // route to display the notes.html file
     app.get("/notes", (req, res) => {
-        console.log("HTML Route to ./public/notes.html");
+        console.log("HTML Route to ../public/notes.html");
 
-        res.sendFile(path.join(__dirname,'./public/notes.html'));
+        res.sendFile(path.join(__dirname,'../public/notes.html'));
         //res.sendFile(path.join(__dirname, "notes.html")); // returns notes.html
     });
 
     // all other routes return index.html
     app.get("*", (req, res) => {
-        console.log("HTML Route for everything else, returns index.html");
-        res.sendFile(path.join(__dirname, "./public/index.html")); // returns index.html
+        console.log("HTML Route for everything else, returns ../public/index.html");
+        res.sendFile(path.join(__dirname, "../public/index.html")); // returns index.html
     });
 }
 
