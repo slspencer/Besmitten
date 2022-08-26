462a2cfc7ff8ea400d93d5d013fd5f8bfb95b157
slpencer <susan.spencer@gmail.com>
2022-08-26 05:01:24 -0500

    use path.join again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,22 +16,25 @@ module.exports = (app) => {
         console.log("HTML Route to ../public/index.html");
         console.log(`__dirname = ${__dirname}`); // __dirname in this file is '/app/routes
         var indexPath = path.join(__dirname, '..', 'public', 'index.html');
-        console.log(`indexPath = ${indexPath}`); // __dirname in this file is '/app/routes
+        console.log(`indexPath = ${indexPath}`); 
         res.sendFile(indexPath);
     });
 
     // route to display the notes.html file
     app.get("/notes", (req, res) => {
         console.log("HTML Route to ../public/notes.html");
-
-        res.sendFile(path.join(__dirname,'../public/notes.html'));
+        var notesPath = path.join(__dirname, '..', 'public', 'notes.html');
+        console.log(`notesPath = ${notesPath}`); 
+        res.sendFile(notesPath);
         //res.sendFile(path.join(__dirname, "notes.html")); // returns notes.html
     });
 
     // all other routes return index.html
     app.get("*", (req, res) => {
         console.log("HTML Route for everything else, returns ../public/index.html");
-        res.sendFile(path.join(__dirname, "../public/index.html")); // returns index.html
+        var defaultPath = path.join(__dirname, '..', 'public', 'index.html');
+        console.log(`defaultPath = ${defaultPath}`);         
+        res.sendFile(defaultPath); // returns index.html
     });
 }
 
